40a695eaa5067161894d17371e5819de2c2ce9c2
Kevin <95513702+TravelerSW@users.noreply.github.com>
2023-05-22 12:37:41 -0300

    Update AlgoritmoRomano.java

--- AlgoritmoRomano.java
+++ AlgoritmoRomano.java
@@ -9,14 +9,51 @@ public class AlgoritmoRomano{
         
         int result = romanToInt(input);
         System.out.println("O número romano é: " + result);
+    }        
 
     public static int romanToInt(String s){
         int ans = 0;
         int prevNum = 0;
 
-        for(i = s.length() - 1; i>=0; i++){
-            
+        for(int i = s.length() - 1; i>=0; i--){
+            int num = getNumericValue(s.charAt(i));
+            if(num<prevNum){
+                ans -= num;
+            } else{
+                ans += num;
+            }
+            prevNum = num;
         }
-    }             
-}
+
+        return ans;
+    }
+    
+    private static int getNumericValue(char c){
+        switch(c){
+            case 'I':
+                return 1;
+
+            case 'V':
+                return 5;
+            
+            case 'X':
+                return 10;
+                
+            case 'L':
+                return 50;
+                
+            case 'C':
+                return 100;
+
+            case 'D':
+                return 500;
+            
+            case 'M':
+                return 1000;
+
+            default:
+                return 0;
+        }
+    }
+
 }
